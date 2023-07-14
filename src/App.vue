13a2f870f59bdfb272e6c75fae5c3664be0c0339
unknown <adamlim2023@gmail.com>
2023-07-15 03:05:13 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,53 @@
 <template>
-  <div id="app">
+  <div id="app" style="display: flex; flex-direction: column; align-items: center;">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <div v-if="account">
-        {{ account }}
-    </div>
-    <button v-else @click="connect">Connect wallet</button>
+    <button @click="connect">Connect wallet</button>
+    {{ account?.address }}
   </div>
 </template>
 
 <script>
-import { EthereumProvider } from "@walletconnect/ethereum-provider";
-const projectId = "496c33146f0305590d925fe8cecab521";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/html";
+import { configureChains, createConfig } from "@wagmi/core";
+import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import { watchAccount } from "@wagmi/core";
 
+const projectId = "496c33146f0305590d925fe8cecab521";
 export default {
   name: "App",
   data: function () {
     return {
-      provider: null,
-      account: null
+      modal: null,
+      account: null,
     };
   },
   async mounted() {
-    this.provider = await EthereumProvider.init({
-      projectId,
-      showQrModal: true,
-      qrModalOptions: { themeMode: "light" },
-      chains: [137, 1, 56],
-      methods: ["eth_sendTransaction", "personal_sign"],
-      events: [
-        "chainChanged",
-        "accountsChanged",
-        "disconnect",
-        "connect",
-        "session_event",
-        "display_uri",
-      ],
-      metadata: {
-        name: "Dev Dapp",
-        description: "My Dapp description",
-        url: "https://my-dapp.com",
-        icons: ["https://my-dapp.com/logo.png"],
-      },
+    const chains = [arbitrum, mainnet, polygon];
+    const { publicClient } = configureChains(chains, [
+      w3mProvider({ projectId }),
+    ]);
+    const wagmiConfig = createConfig({
+      autoConnect: true,
+      connectors: w3mConnectors({ projectId, chains }),
+      publicClient,
     });
-    if (this.provider.accounts[0]) {
-      this.account = this.provider.accounts[0];
-    }
-    console.log("provider", this.provider);
+    const ethereumClient = new EthereumClient(wagmiConfig, chains);
+    this.modal = await new Web3Modal({ projectId }, ethereumClient);
+    watchAccount((account) => {
+      console.log(account);
+      this.account = account;
+    });
   },
   methods: {
     connect: async function () {
-      try {
-        if (this.provider) {
-          await this.provider.enable();
-
-          if (this.provider.accounts[0]) {
-            this.account = this.provider.accounts[0];
-          }
-        }
-      } catch (err) {
-        console.error(err);
-      } finally {
-        console.log("finally");
-      }
+      await this.modal.openModal();
     },
-  }
+  },
 };
 </script>
 
